4080c0c32dae05ab7661b495d8313989e088cb05
CaptianObvious <129606617+CaptianObvious@users.noreply.github.com>
2025-01-30 14:25:41 +0500

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,11 +1,8 @@
-function Jobs_desk() {
-    let job = document.querySelectorAll('.jobs');
-    for (let i = 1; i <= 5; i++){
-        var vacancy = [];
-        const min = 1;
-        const max = 121;
-        const random_brigade = Math.floor(Math.random() * (max - min + 1)) + min;
-        vacancy.push(random_brigade);
-        jobs.innerHTML[i] = vacancy[i];}
-Jobs_desk();
-} 
+let job = document.querySelectorAll('.jobs');
+for (let i = 1; i <= 5; i++){
+    var vacancy = [];
+    const min = 1;
+    const max = 121;
+    const random_brigade = Math.floor(Math.random() * (max - min + 1)) + min;
+    vacancy.push(random_brigade);
+    jobs.innerHTML[i] = vacancy[i];}
