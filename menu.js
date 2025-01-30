a9c910d8a775ef5e3c618e4bb5dddcbb7aa4fadb
CaptianObvious <129606617+CaptianObvious@users.noreply.github.com>
2025-01-30 09:22:07 +0500

    Update menu.js

---
+++ menu.js
@@ -0,0 +1,7 @@
+function Jobs_desk() {
+    let job = document.querySelectorAll('.jobs');
+    const vacancy = [];
+    const min = 1;
+    const max = 10;
+    const randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
+}
