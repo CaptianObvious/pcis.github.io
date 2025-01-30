c0d178f1091d46f5ae2911cee321b2e19267ae96
CaptianObvious <129606617+CaptianObvious@users.noreply.github.com>
2025-01-30 14:12:15 +0500

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,7 +1,7 @@
 function Jobs_desk() {
     let job = document.querySelectorAll('.jobs');
     for (let i = 1; i <= 5; i++){
-        const vacancy = [];
+        var vacancy = [];
         const min = 1;
         const max = 121;
         const random_brigade = Math.floor(Math.random() * (max - min + 1)) + min;
